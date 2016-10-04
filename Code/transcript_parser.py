4cdbb0572b410d59db022f58255edc6542d8d7d9
Scotty Huhn <scotty.huhn@gmail.com>
2016-10-04 10:42:01 -0700

    10/4 Update

--- Code/transcript_parser.py
+++ Code/transcript_parser.py
@@ -3,6 +3,8 @@ import re
 import itertools
 import string
 import operator
+from textblob import TextBlob
+from summa import summarizer
 
 def setup_buckets(file_name):
     """
@@ -63,11 +65,64 @@ def frequency(key_players_textdict, output_counter):
         print tuple1[0], tuple1[1], round(float(tuple1[1]) / (total_interventions_sum), 2)
     print ""
 
+def sentiment_analysis(key_players_textdict):
+    for candidate, list_words in key_players_textdict.iteritems():
+        string_words = (' '.join(list_words))
+        textblob = TextBlob(string_words)
+        print candidate, textblob.sentiment
+
+def summarize_speech(key_players_textdict, defaults = ['CLINTON:', 'TRUMP:']):
+    # for candidate, list_words in key_players_textdict.iteritems():
+    for candidate in defaults:
+        list_words = key_players_textdict[candidate]
+        string_words = (' '.join(list_words))
+        print candidate
+        print summarizer.summarize(string_words, words = 200)
+        print "-----"
+
+def topic_modeling(relevant_debates, defaults = ['CLINTON:', 'TRUMP:'])):
+    #aggregate dictionaries
+    aggregate_dict = {}
+    for d in relevant_debates:
+      for k, v in d.iteritems():
+        if aggregate_dict.has_key(k):
+          aggregate_dict[k] = aggregate_dict[k] + v
+        else:
+          aggregate_dict[k] = v
+
+    #setup df - column = candidate name, rows = sentences
+
+
+    for candidate in defaults:
+        sentences = []
+        list_words = key_players_textdict[candidate]
+        string_words = (' '.join(list_words))
+        textblob = TextBlob(string_words)
+        for sentence in textblob.sentence:
+            sentences.append(sentence)
+        df = DataFrame(sentences, columns = candidate)
+
+
+
+
 if __name__ == '__main__':
-    all_files = file_grab("G")
+    all_files = file_grab('G')
+    relevant_debates = []
     for name, file_name in all_files.iteritems():
         m_t, k_p = setup_buckets(file_name)
         mods, parts, key_players, output_counter = parse_names(k_p)
         key_players_textdict, output_counter = parse_text(m_t, key_players, output_counter)
-        #frequency(key_players_textdict, output_counter)
-        
+
+        #SUMMARIES
+        # print name
+        # summarize_speech(key_players_textdict)
+        # print "-----"
+
+        #SENTIMENT ANALYSIS
+        # print name
+        # sentiment_analysis(key_players_textdict)
+        # print "------"
+
+        relevant_debates.append(key_players_textdict)
+
+    #TOPIC MODELING
